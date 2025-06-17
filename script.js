8b4c5c5586801a6d9f0b5ca451716c0e386538a1
Elen <elen.v.grigoryan@gmail.com>
2025-06-17 21:04:59 +0400

    added keypress enter

--- script.js
+++ script.js
@@ -3,28 +3,67 @@ let message=document.querySelector("#message")
 let randomNum=Math.floor(Math.random()*100+1) //[1,101)
 let guesses=0
 guessbtn.addEventListener('click', function(){
-    let userInp=parseInt(document.querySelector("#userInp").value)
-    if(isNaN(userInp) || userInp<1 || userInp>100){
-        message.innerHTML="Please insert number between 1 and 100"
-        message.style.color="red"
-        guesses++
-    }
-    else if(userInp<randomNum){
-        message.innerHTML="Too low, try again"
-        message.style.color="red"
-        guesses++
+    // let userInp=parseInt(document.querySelector("#userInp").value)
+    // if(isNaN(userInp) || userInp<1 || userInp>100){
+    //     message.innerHTML="Please insert number between 1 and 100"
+    //     message.style.color="red"
+    //     guesses++
+    // }
+    // else if(userInp<randomNum){
+    //     message.innerHTML="Too low, try again"
+    //     message.style.color="red"
+    //     guesses++
 
-    }
-    else if(userInp>randomNum){
-        message.innerHTML="Too high, try again"
-        message.style.color="red"
-        guesses++
+    // }
+    // else if(userInp>randomNum){
+    //     message.innerHTML="Too high, try again"
+    //     message.style.color="red"
+    //     guesses++
 
-    }
-    else{
-        message.innerHTML="Congradulations, you guessed."
-        message.style.color="green"
-        guesses++
-        message.innerHTML+=` It took ${guesses} times to guess the number`
-    }
+    // }
+    // else{
+    //     message.innerHTML="Congradulations, you guessed."
+    //     message.style.color="green"
+    //     guesses++
+    //     message.innerHTML+=` It took ${guesses} times to guess the number`
+    // }
+    guessAction()
 })
+
+
+//new 
+const userInp=document.querySelector("#userInp")
+userInp.addEventListener("keypress",(e)=>{
+   if(e.key=="Enter"){
+       guessAction()
+   } 
+});
+
+
+
+  function guessAction(){
+      let userInp = parseInt(document.querySelector("#userInp").value)
+      if (isNaN(userInp) || userInp < 1 || userInp > 100) {
+          message.innerHTML = "Please insert number between 1 and 100"
+          message.style.color = "red"
+          guesses++
+      }
+      else if (userInp < randomNum) {
+          message.innerHTML = "Too low, try again"
+          message.style.color = "red"
+          guesses++
+
+      }
+      else if (userInp > randomNum) {
+          message.innerHTML = "Too high, try again"
+          message.style.color = "red"
+          guesses++
+
+      }
+      else {
+          message.innerHTML = "Congradulations, you guessed."
+          message.style.color = "green"
+          guesses++
+          message.innerHTML += ` It took ${guesses} times to guess the number`
+      }
+  }  
